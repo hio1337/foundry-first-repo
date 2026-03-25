57818af2415a2256f9a0d302a56f4fdfde2820de
csgop <andreypryakhin69@gmail.com>
2026-03-25 21:09:03 +1000

    chore: run forge fmt

--- script/DeployFundeMe.s.sol
+++ script/DeployFundeMe.s.sol
@@ -7,14 +7,13 @@ import {FundMe} from "../src/FundMe.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
-  function run() external returns (FundMe) {
-    
-    HelperConfig helperConfig = new HelperConfig();
-    address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+    function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
 
-    vm.startBroadcast();
-    FundMe fundMe = new FundMe(ethUsdPriceFeed);
-    vm.stopBroadcast();
-    return fundMe;
-  }
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return fundMe;
+    }
 }
--- test/mocks/MockV3Aggregator.sol
+++ test/mocks/MockV3Aggregator.sol
@@ -1,4 +1,4 @@
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
